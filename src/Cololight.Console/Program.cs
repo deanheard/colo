9cb3b73121df35f2001c4800a33fccfe3433c4dc
deanheard <65692118+deanheard@users.noreply.github.com>
2020-11-24 18:36:53 +1300

    Commit of existing source code.

---
+++ src/Cololight.Console/Program.cs
@@ -0,0 +1,145 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Drawing;
+using System.Net;
+using System.Reflection;
+
+namespace Cololight.Console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            PrintHeader();
+
+            try
+            {
+                int returnCode = 0;
+                var app = new CommandLineApplication();
+
+                app.HelpOption();
+                var optionHost = app.Option("-a|--address <IPADDRESS>", "The IP address of the Cololight device you want to instruct", CommandOptionType.SingleValue);
+                var optionOn = app.Option<bool>("-o|--on <BOOL>", "True to turn the light on, false to turn it off", CommandOptionType.SingleOrNoValue);
+                var optionColour = app.Option<KnownColor>("-c|--colour <COLOUR>", "Sets the base colour to one of the System.Drawing.Color types", CommandOptionType.SingleOrNoValue);
+                var optionRgb = app.Option("-rgb|--rgb <RRGGBB>", "Sets the base colour a custom HTML defined colour", CommandOptionType.SingleOrNoValue);
+                var optionEffect = app.Option<CololightController.CololightController.Effects>("-e|--effect <EFFECT>", "Sets the animated Effect of the lights", CommandOptionType.SingleOrNoValue);
+                var optionBrightness = app.Option<int>("-b|--brightness <N>", "Sets the brightness as a percentage between 0 and 100", CommandOptionType.SingleOrNoValue);
+
+                app.OnExecute(() =>
+                {
+                    CololightController.CololightController controller = new CololightController.CololightController();
+
+                    if (optionHost.HasValue())
+                    {
+                        IPAddress address = IPAddress.Parse(optionHost.Value());
+                        System.Console.WriteLine(string.Format("Sending commands to IP Address {0}", address.ToString()));
+                        controller.Host = address;
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException("--address");
+                    }
+
+                    if (optionColour.HasValue() && returnCode == 0)
+                    {
+                        var colour = Color.FromKnownColor(optionColour.ParsedValue);
+                        System.Console.WriteLine(string.Format("Setting Colour to {0}", colour.Name)); ;                        
+                        returnCode = controller.SetColour(colour);
+                    }
+
+                    if (optionRgb.HasValue() && returnCode == 0)
+                    {
+                        var rgb = optionRgb.Value();
+                        System.Console.WriteLine(string.Format("Setting Colour to #{0}", rgb));
+                        returnCode = controller.SetColour(rgb);
+                    }
+
+                    if (optionEffect.HasValue() && returnCode == 0)
+                    {
+                        var effect = optionEffect.ParsedValue;
+                        System.Console.WriteLine(string.Format("Setting Effect to {0}", effect));
+                        returnCode = controller.SetEffect(effect);
+                    }
+
+                    if (optionBrightness.HasValue() && returnCode == 0)
+                    {
+                        var brightness = optionBrightness.ParsedValue;
+
+                        if (brightness < 0)
+                            brightness = 0;
+                        else if (brightness > 100)
+                            brightness = 100;
+
+                        System.Console.WriteLine(string.Format("Setting brightness to {0}", brightness));
+                        returnCode = controller.SetBrightness(brightness);
+                    }
+
+                    if (optionOn.HasValue() && returnCode == 0)
+                    {
+                        var on = optionOn.HasValue() ? optionOn.ParsedValue : true;
+
+                        if (on)
+                        {
+                            System.Console.WriteLine("Turning On");
+                            returnCode = controller.TurnOn();
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Turning Off");
+                            returnCode = controller.TurnOff();
+                        }
+                    }
+
+                    if (returnCode == -1)
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Red;
+                        System.Console.WriteLine("ERROR: Unable to find device on the network");
+                        System.Console.ResetColor();
+                    }
+
+                    return returnCode;
+                });
+
+                return app.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.WriteLine(ex.Message);
+                System.Console.ResetColor();
+                return -2;
+            }
+        }   
+        
+        private static void PrintHeader()
+        {
+            System.Console.ResetColor();
+
+            System.Console.WriteLine("");
+
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Write("=");
+
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.Write("=");
+
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.Write("=");
+
+            System.Console.Write(" Cololight Controller v{0} ", Assembly.GetEntryAssembly().GetName().Version);
+
+            System.Console.ForegroundColor = ConsoleColor.Blue;
+            System.Console.Write("=");
+
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.Write("=");
+
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            System.Console.Write("=");
+
+            System.Console.ResetColor();
+
+            System.Console.Write("\r\n");
+        }
+    }
+}
---
+++ src/CololightController/CololightController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace CololightController
+{
+    public class CololightController
+    {
+        private const string COMMAND_PREFIX = "535a30300000000000";
+        private const string COMMAND_CONFIG = "20000000000000000000000000000000000100000000000000000004010301c";
+        private const string COMMAND_EFFECT = "23000000000000000000000000000000000100000000000000000004010602ff";
+
+        private const string COMMAND_DISTANCE = "2300000000000000000000000000000000010000000000000000000b01068700";
+        private const int port = 8900;
+                
+        public IPAddress Host { get; set; }
+
+        public enum Effects
+        {
+            Club80s,
+            CherryBlossom,
+            CocktailParade,
+            Instagrammer,
+            Pensieve,
+            Savasana,
+            Sunrise,
+            TheCircus,
+            Unicorns,
+            Christmas,
+            RainbowFlow,
+            MusicMode
+        }
+
+        private NameValueCollection EffectsLookup = new NameValueCollection();
+
+        public CololightController()
+        {
+            EffectsLookup.Add("Club80s", "049a0000");
+            EffectsLookup.Add("CherryBlossom", "04940800");
+            EffectsLookup.Add("CocktailParade", "05bd0690");
+            EffectsLookup.Add("Instagrammer", "03bc0190");
+            EffectsLookup.Add("Pensieve", "04c40600");
+            EffectsLookup.Add("Savasana", "04970400");
+            EffectsLookup.Add("Sunrise", "01c10a00");
+            EffectsLookup.Add("TheCircus", "04810130");
+            EffectsLookup.Add("Unicorns", "049a0e00");
+            EffectsLookup.Add("Christmas", "068b0900");
+            EffectsLookup.Add("RainbowFlow", "03810690");
+            EffectsLookup.Add("MusicMode", "07bd0990");
+        }
+
+        public int TurnOn()
+        {
+            string command = string.Format("{0}{1}{2}", COMMAND_PREFIX, COMMAND_CONFIG, "f35");
+
+            return SendMessage(command);
+        }
+
+        public int TurnOff()
+        {
+            string command = string.Format("{0}{1}{2}", COMMAND_PREFIX, COMMAND_CONFIG, "e1e");
+
+            return SendMessage(command);
+        }
+
+        //public int DistanceTest()
+        //{
+        //    string command = string.Format("{0}{1}{2}", COMMAND_PREFIX, COMMAND_DISTANCE, "85008503");
+
+        //    return SendMessage(command);
+        //}
+
+        public int SetEffect(Effects effect)
+        {
+            string effectValue = EffectsLookup.Get(Enum.GetName(typeof(Effects), effect));
+
+            string command = string.Format("{0}{1}{2}", COMMAND_PREFIX, COMMAND_EFFECT, effectValue);
+
+            return SendMessage(command);
+        }
+
+        public int SetBrightness(int brightness)
+        {
+            if (brightness < 0)
+                brightness = 0;
+            else if (brightness > 100)
+                brightness = 100;
+
+            string prefix = "f";
+            string command = string.Format("{0}{1}{2}{3}", COMMAND_PREFIX, COMMAND_CONFIG, prefix, brightness.ToString("X2"));
+
+            return SendMessage(command);
+        }
+
+        public int SetColour(System.Drawing.Color colour)
+        {
+            string hexColour = colour.R.ToString("X2") + colour.G.ToString("X2") + colour.B.ToString("X2");
+
+            return this.SetColour(hexColour);
+        }
+
+        public int SetColour(string colour)
+        {
+            string prefix = "00";
+            string command = string.Format("{0}{1}{2}{3}", COMMAND_PREFIX, COMMAND_EFFECT, prefix, colour);
+
+            return SendMessage(command);
+        }
+
+        private int SendMessage(string message)
+        {            
+            if (PingHost(this.Host.ToString()))
+            {
+                UdpClient udpClient = new UdpClient();
+                IPEndPoint ipEndPoint = new IPEndPoint(this.Host, port);
+
+                try
+                {
+                    udpClient.Connect(ipEndPoint);
+
+                    byte[] dgram = StringToByteArray(message);
+
+                    if (dgram.Length > 0)
+                    {
+                        udpClient.Send(dgram, dgram.Length);
+                        return 0;
+                    }
+                }
+                catch (Exception e)
+                {                    
+                    Debug.WriteLine(e.ToString());
+                }
+                finally
+                {
+                    udpClient.Close();
+                }
+            }            
+
+            return -1;
+        }
+
+        private bool PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+
+            try
+            {
+                pinger = new Ping();
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+                // Discard PingExceptions and return false;
+            }
+            finally
+            {
+                if (pinger != null)
+                {
+                    pinger.Dispose();
+                }
+            }
+
+            return pingable;
+        }
+
+        private byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+
+            if (NumberChars % 2 == 0)
+            {
+                byte[] bytes = new byte[NumberChars / 2];
+                for (int i = 0; i < NumberChars; i += 2)
+                    bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+                return bytes;
+            }
+            else
+            {                
+                return new byte[0];
+            }
+        }
+    }
+}
